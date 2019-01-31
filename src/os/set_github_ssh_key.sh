6ef3ee15bdfd7a8340982487a8037653d1029c0b
Daniel Comnea <dcomnea@synamedia.com>
2019-01-31 00:48:10 +0000

    Disable github ssh testing connection

--- src/os/set_github_ssh_key.sh
+++ src/os/set_github_ssh_key.sh
@@ -78,8 +78,8 @@ set_github_ssh_key() {
     generate_ssh_keys "$sshKeyFileName"
     add_ssh_configs "$sshKeyFileName"
     copy_public_ssh_key_to_clipboard "${sshKeyFileName}.pub"
-    open_github_ssh_page
-    test_ssh_connection \
+    # open_github_ssh_page
+    # test_ssh_connection \
         && rm "${sshKeyFileName}.pub"
 
 }
